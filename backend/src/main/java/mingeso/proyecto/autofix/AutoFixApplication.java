023b837a3791c3ad5033ad0ee0b6af343ed1eae0
Esteban Glochon <esteban.glochon@usach.cl>
2024-04-04 19:16:06 -0300

    init backend

---
+++ backend/src/main/java/mingeso/proyecto/autofix/AutoFixApplication.java
@@ -0,0 +1,13 @@
+package mingeso.proyecto.autofix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoFixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoFixApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/mingeso/proyecto/autofix/AutoFixApplicationTests.java
@@ -0,0 +1,13 @@
+package mingeso.proyecto.autofix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoFixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
