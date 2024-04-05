b593a006f70234c9dd19991386f68f38ea021d68
Esteban Glochon <esteban.glochon@usach.cl>
2024-04-05 00:01:22 -0300

    added frontend service in containers stack

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
+    proxy: {
+      '/api': process.env.BACKEND_URI ? process.env.BACKEND_URI : 'http://localhost:8080',
+    }
+  },
   plugins: [react()],
 })
